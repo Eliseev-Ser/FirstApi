76f5a4904529537fc89a01d3f5d236722b700fee
Eliseev-Ser <eliseev.sereja@icloud.com>
2023-11-01 15:33:19 +0600

    Initial Commit

---
+++ FirstApi/FirstApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApiApp.swift
+//  FirstApi
+//
+//  Created by Сергей Елисеев on 01.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
