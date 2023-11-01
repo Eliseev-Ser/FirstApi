b16d3f08b56fa5c8694a91c02180cd1d22fe2154
Eliseev-Ser <eliseev.sereja@icloud.com>
2023-11-01 15:35:29 +0600

    new commit

--- FirstApi/ContentView.swift
+++ FirstApi/ContentView.swift
@@ -1,26 +1,64 @@
+
+
 //
 //  ContentView.swift
-//  FirstApi
+//  MyFirstProject
 //
-//  Created by Сергей Елисеев on 01.11.2023.
+//  Created by Сергей Елисеев on 20.09.2023.
 //
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State var posts: [Post] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(posts) {post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .fontWeight(.bold)
+                    Text(post.body)
+                    //
+                }
+            }.onAppear() {
+                Api().getPost { (posts) in
+                    self.posts = posts
+                }
+            }
+            .navigationBarTitle("Posts")
         }
-        .padding()
     }
 }
+
+struct Post: Codable, Identifiable {
+    var id: Int
+    var title: String
+    var body: String
+}
+
+class Api {
+    func getPost(completion: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            
+            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+        }
+        .resume()
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
